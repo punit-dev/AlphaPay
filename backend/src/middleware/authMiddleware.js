cfb874c1f4d91e7baaa5381555e30c3997979b81
punit-dev <punitpoddar2310@gmail.com>
2025-07-05 22:39:50 +0530

    Add authentication middleware to verify user tokens and handle errors

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const { verifyToken } = require("../util/token");
+const asyncHandler = require("express-async-handler");
+const UserModel = require("../models/userModel");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  const token = req.cookies.token;
+
+  let verify;
+  try {
+    verify = verifyToken(token);
+  } catch (error) {
+    res.status(401);
+    throw new Error("Token invalid.");
+  }
+
+  const user = await UserModel.findById(verify.userID);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+
+  req.user = user;
+  next();
+});
+
+module.exports = authMiddleware;
